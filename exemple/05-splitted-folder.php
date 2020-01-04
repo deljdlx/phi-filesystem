cc67c2c90eaf2df6ae846aa0e62975598cfec89e
Julien - Biniou <julien.delsescaux@oclock.io>
2020-01-04 21:37:18 +0100

    sync

--- exemple/05-splitted-folder.php
+++ exemple/05-splitted-folder.php
@@ -5,5 +5,21 @@ require(__DIR__.'/_autoload.php');
 use Phi\FileSystem\SplittedFolder;
 
 $path = new SplittedFolder(__DIR__);
-$path->filePutContents('test.php', 'hello world');
+
+
+
+$path->filePutContents('test.txt', 'hello world');
+
+echo $path->fileGetContents('test.txt');
+
+echo "\n";
+
+foreach ($path->getFile() as $file) {
+    echo $file;
+    echo "\n";
+}
+
+
+
+//
 
--- source/class/SplittedFolder.php
+++ source/class/SplittedFolder.php
@@ -5,40 +5,83 @@ namespace Phi\FileSystem;
 class SplittedFolder
 {
 
-    private $rootPath;
+    private $sourcePath;
     private $depth;
+    private $threshold;
 
-    public function __construct($rootPath, $depth = 4)
+    public function __construct($sourcePath, $depth = 4, $threshold = 2)
     {
-        $this->rootPath = $rootPath;
-        $this->depth =$depth;
+        $this->sourcePath = $sourcePath;
+        $this->depth = $depth;
+        $this->threshold = $threshold;
     }
 
 
     public function filePutContents($file, $content, $option = null)
     {
+        $file = $this->normalizeFileName($file);
         $tree = $this->generateTree($file);
 
-        $path = $this->rootPath;
+        $path = $this->sourcePath;
         foreach ($tree as $part) {
-            $path .= '/'.$part;
-            mkdir($path);
+            $path .= '/' . $part;
+            if(!is_dir($path)) {
+                mkdir($path);
+            }
+
         }
+        file_put_contents($path . '/' . $file, $content, $option);
+        return $this;
+    }
 
 
+    public function fileGetContents($file)
+    {
+        $file = $this->normalizeFileName($file);
+        $tree = $this->generateTree($file);
+        $path = $this->sourcePath.'/'.implode('/', $tree);
+
+        return file_get_contents($path.'/'.$file);
+    }
+
+
+    public function getFile()
+    {
+        $directory = new \RecursiveDirectoryIterator($this->sourcePath);
+        $iterator = new \RecursiveIteratorIterator($directory);
+
+        foreach ($iterator as $info) {
+            $file = $info->getPathName();
+            $fileName = basename($file);
+            if($fileName != '.' && $fileName !='..' && is_file($file)) {
+                yield $file;
+            }
+        }
     }
 
 
     public function generateTree($file)
     {
-        $hash = md5($file);
 
-        $tree =[];
-        for($i = 0 ; $i<$this->depth; $i++) {
-            $tree[] = $hash{$i};
+        $hash = md5($file) . sha1($file);
+
+        $chunk = chunk_split($hash, $this->threshold, "\n");
+        $parts = explode("\n", $chunk);
+
+        $tree = [];
+        for ($i = 0; $i < $this->depth; $i++) {
+            $tree[] = $parts[$i];
         }
 
         return $tree;
+    }
+
+    protected function normalizeFileName($fileName)
+    {
+        $fileName = trim($fileName);
+        $fileName = preg_replace('`^/+`', '', $fileName);
+        $fileName = preg_replace('`/+$`', '', $fileName);
+        return $fileName;
     }
 
 
